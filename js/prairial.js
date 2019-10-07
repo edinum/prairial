ce8ab82e87d71a92228ea6d06c29c522efe5367e
Thomas Brouard <contact@brrd.fr>
2019-10-07 19:20:34 +0200

    Catalogue: add (cheap) transition

--- js/prairial.js
+++ js/prairial.js
@@ -1,28 +1,36 @@
 // Prairial JS
 
 (function() {
+  var transitionDuration = 500;
+
   // Filtres catalogue
   function updateFilters() {
     var selectedTags = [];
-    $(".filter-control").each(function () {
+    $(".filter-control").each(function() {
       if (!this.value) return;
       selectedTags.push(this.value);
     });
 
-    if (selectedTags.length === 0) {
-      $(".revue").removeClass("hidden");
-      return;
-    }
-
-    $(".revue").each(function () {
+    var visible = [];
+    var hidden = [];
+    $(".revue").each(function() {
       var tags = $(this).attr("data-tags").split(",");
-      var keepVisible = selectedTags.every(function (value) {
+      var keepVisible = selectedTags.every(function(value) {
         return tags.indexOf(value) > -1;
       });
-      $(this).toggleClass("hidden", !keepVisible);
+      (keepVisible ? visible : hidden).push($(this));
     });
+    
+    (visible.length === 0 ? visible : hidden).push($(".revues-aucun-resultat"));
 
-    $(".revues-aucun-resultat").toggleClass("hidden", $(".revue:not(.hidden)").length > 0);
+    var $hidden = $(hidden).map(function () { return this.toArray(); });
+    var $visible = $(visible).map(function () { return this.toArray(); });
+
+    $hidden.hide(transitionDuration, function() {
+      window.setTimeout(function() {
+        $visible.show(transitionDuration);
+      }, transitionDuration);
+    });
   }
 
   function resetFilters() {
