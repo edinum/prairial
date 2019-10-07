ee2da1c1c5a808c68edd57d82c72c3dbc94dcf32
Thomas Brouard <contact@brrd.fr>
2019-10-07 17:45:55 +0200

    JS: avoid global scope polution

--- js/prairial.js
+++ js/prairial.js
@@ -1,35 +1,37 @@
 // Prairial JS
 
-// Filtres catalogue
-function updateFilters () {
-  var selectedTags = [];
-  $(".filter-control").each(function () {
-    if (!this.value) return;
-    selectedTags.push(this.value);
-  });
+(function() {
+  // Filtres catalogue
+  function updateFilters() {
+    var selectedTags = [];
+    $(".filter-control").each(function () {
+      if (!this.value) return;
+      selectedTags.push(this.value);
+    });
 
-  if (selectedTags.length === 0) {
-    $(".revue").removeClass("hidden");
-    return;
+    if (selectedTags.length === 0) {
+      $(".revue").removeClass("hidden");
+      return;
+    }
+
+    $(".revue").each(function () {
+      var tags = $(this).attr("data-tags").split(",");
+      var keepVisible = selectedTags.every(function (value) {
+        return tags.indexOf(value) > -1;
+      });
+      $(this).toggleClass("hidden", !keepVisible);
+    });
+
+    $(".revues-aucun-resultat").toggleClass("hidden", $(".revue:not(.hidden)").length > 0);
   }
 
-  $(".revue").each(function () {
-    var tags = $(this).attr("data-tags").split(",");
-    var keepVisible = selectedTags.every(function (value) {
-      return tags.indexOf(value) > -1;
-    });
-    $(this).toggleClass("hidden", !keepVisible);
+  function resetFilters() {
+    $(".filter-control").val("").change();
+  }
+
+  $(function () {
+    updateFilters();
+    $(".filter-control").on("change", updateFilters);
+    $("#filter-reset").on("click", resetFilters);
   });
-
-  $(".revues-aucun-resultat").toggleClass("hidden", $(".revue:not(.hidden)").length > 0);
-}
-
-function resetFilters () {
-  $(".filter-control").val("").change();
-}
-
-$(function() {
-  updateFilters();
-  $(".filter-control").on("change", updateFilters);
-  $("#filter-reset").on("click", resetFilters);
-});
+})();
