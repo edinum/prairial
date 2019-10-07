b870994685262dcb4766951f6418d3dbb22b6c06
Thomas Brouard <contact@brrd.fr>
2019-10-07 19:24:15 +0200

    Revert "Catalogue: add (cheap) transition"
    This reverts commit ce8ab82e87d71a92228ea6d06c29c522efe5367e.

--- js/prairial.js
+++ js/prairial.js
@@ -1,36 +1,28 @@
 // Prairial JS
 
 (function() {
-  var transitionDuration = 500;
-
   // Filtres catalogue
   function updateFilters() {
     var selectedTags = [];
-    $(".filter-control").each(function() {
+    $(".filter-control").each(function () {
       if (!this.value) return;
       selectedTags.push(this.value);
     });
 
-    var visible = [];
-    var hidden = [];
-    $(".revue").each(function() {
+    if (selectedTags.length === 0) {
+      $(".revue").removeClass("hidden");
+      return;
+    }
+
+    $(".revue").each(function () {
       var tags = $(this).attr("data-tags").split(",");
-      var keepVisible = selectedTags.every(function(value) {
+      var keepVisible = selectedTags.every(function (value) {
         return tags.indexOf(value) > -1;
       });
-      (keepVisible ? visible : hidden).push($(this));
+      $(this).toggleClass("hidden", !keepVisible);
     });
-    
-    (visible.length === 0 ? visible : hidden).push($(".revues-aucun-resultat"));
 
-    var $hidden = $(hidden).map(function () { return this.toArray(); });
-    var $visible = $(visible).map(function () { return this.toArray(); });
-
-    $hidden.hide(transitionDuration, function() {
-      window.setTimeout(function() {
-        $visible.show(transitionDuration);
-      }, transitionDuration);
-    });
+    $(".revues-aucun-resultat").toggleClass("hidden", $(".revue:not(.hidden)").length > 0);
   }
 
   function resetFilters() {
